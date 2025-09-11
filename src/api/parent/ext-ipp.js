922c6050b9810a8903b6e9bd9752729f40390258
Andrea Marchesini <amarchesini@mozilla.com>
2025-09-11 06:44:56 +0200

    First commit

---
+++ src/api/parent/ext-ipp.js
@@ -0,0 +1,115 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const lazy = {};
+ChromeUtils.defineESModuleGetters(lazy, {
+  IPProtectionService: 'resource:///modules/ipprotection/IPProtectionService.sys.mjs',
+  ExtensionCommon: 'resource://gre/modules/ExtensionCommon.sys.mjs',
+});
+
+this.ippActivator = class extends ExtensionAPI {
+  onStartup() {}
+
+  onShutdown(isAppShutdown) {}
+
+  getAPI(context) {
+    return {
+      ippActivator: {
+        onIPPActivated: new lazy.ExtensionCommon.EventManager({
+          context,
+          name: 'ippActivator.onIPPActivated',
+          register: (fire) => {
+            const topic = 'IPProtectionService:Started';
+            const observer = {
+              observe(subject, t, data) {
+                if (t === topic) {
+                  fire.async({});
+                }
+              },
+            };
+            Services.obs.addObserver(observer, topic);
+            return () => {
+              Services.obs.removeObserver(observer, topic);
+            };
+          },
+        }).api(),
+        isIPPActive() {
+          return !!lazy.IPProtectionService.connection;
+        },
+        async showMessage(message) {
+          return new Promise((resolve) => {
+            try {
+              const win = Services.wm.getMostRecentWindow('navigator:browser');
+              if (!win || !win.gBrowser) {
+                resolve('closed');
+                return;
+              }
+
+              const nbox = win.gBrowser.getNotificationBox();
+              const id = 'ipp-activator-notification';
+
+              const existing = nbox.getNotificationWithValue?.(id);
+              if (existing) {
+                nbox.removeNotification(existing);
+              }
+
+              let settled = false;
+              const done = (result) => {
+                if (settled) return;
+                settled = true;
+                try {
+                  const cur = nbox.getNotificationWithValue?.(id);
+                  if (cur) nbox.removeNotification(cur);
+                } catch (e) {
+                  console.log('Unable to remove previous notifications', e);
+                }
+                resolve(result);
+              };
+
+              const buttons = [
+                {
+                  label: 'OK',
+                  accessKey: 'O',
+                  callback() {
+                    done('clicked');
+                    return true;
+                  },
+                },
+                {
+                  label: "Don't show again",
+                  accessKey: 'D',
+                  callback() {
+                    done('not-anymore');
+                    return true;
+                  },
+                },
+              ];
+
+              nbox.appendNotification(
+                id,
+                {
+                  label: message || '',
+                  priority: nbox.PRIORITY_INFO_HIGH,
+                  eventCallback: (event) => {
+                    if (event === 'dismissed') {
+                      done('closed');
+                    }
+                  },
+                },
+                buttons,
+              );
+            } catch (e) {
+              console.log('Unable to show the message', e);
+              resolve('closed');
+            }
+          });
+        },
+        async allowURL(url) {
+          if (!lazy.IPProtectionService.connection) return;
+          lazy.IPProtectionService.connection.addPageExclusion(url);
+        },
+      },
+    };
+  }
+};
---
+++ src/bg.js
@@ -0,0 +1,101 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const BREAKAGES = [
+  {
+    id: 'youtube',
+    domains: ['youtube.com', 'example.com'],
+    message:
+      'Firefox VPN could break the YouTube video playing. Click here to disable Firefox VPN for this domain.',
+  },
+];
+
+class IPPAddonActivator {
+  #ignoredBreakages;
+
+  constructor() {
+    this.#ignoredBreakages = new Set();
+
+    this.tabUpdated = this.#tabUpdated.bind(this);
+
+    browser.ippActivator.isIPPActive().then(() => this.#init());
+    browser.ippActivator.onIPPActivated.addListener((enabled) => {
+      if (enabled) {
+        this.#init();
+      } else {
+        this.#uninit();
+      }
+    });
+  }
+
+  async #init() {
+    await this.#loadIgnored();
+
+    browser.tabs.onUpdated.addListener(this.tabUpdated, { properties: ['url'] });
+  }
+
+  async #uninit() {
+    browser.tabs.onUpdated.removeListener(this.tabUpdated);
+  }
+
+  async #tabUpdated(tabId, changeInfo, tab) {
+    const domain = this.#retrieveDomainFromTab(tab);
+    if (domain === '') return;
+
+    const breakage = BREAKAGES.find((breakage) => breakage.domains.includes(domain));
+    if (!breakage) return;
+
+    if (this.#ignoredBreakages.has(breakage.id)) return;
+
+    const answer = await browser.ippActivator.showMessage(breakage.message);
+    switch (answer) {
+      case 'closed':
+        break;
+
+      case 'not-anymore':
+        this.#ignoredBreakages.add(breakage.id);
+        this.#persistIgnored();
+        break;
+
+      case 'clicked':
+        await browser.ippActivator.allowURL(tab.url);
+        await browser.tabs.reload(tabId, { bypassCache: true });
+        break;
+
+      default:
+        console.log('Unexpected result:', answer);
+        break;
+    }
+  }
+
+  async #loadIgnored() {
+    try {
+      const { ignoredBreakages } = await browser.storage.local.get('ignoredBreakages');
+      if (Array.isArray(ignoredBreakages)) {
+        this.#ignoredBreakages = new Set(ignoredBreakages);
+      }
+    } catch (_) {
+      // Nothing to do here. The storage does not exist yet.
+    }
+  }
+
+  async #persistIgnored() {
+    try {
+      await browser.storage.local.set({ ignoredBreakages: Array.from(this.#ignoredBreakages) });
+    } catch (e) {
+      console.log('Unable to store the ignored-breakagees');
+    }
+  }
+
+  #retrieveDomainFromTab(tab) {
+    try {
+      const u = new URL(tab.url);
+      return u.hostname;
+    } catch (e) {
+      return '';
+    }
+  }
+}
+
+new IPPAddonActivator();
